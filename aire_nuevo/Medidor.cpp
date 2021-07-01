afd8262c1a5ce4ef6141a723f0ba3f3dcae0e7c8
LeandroAngeli <leandroangeli@outlook.com.ar>
2021-07-01 15:47:03 -0300

    Cargado hasta la placa 0546

--- aire_nuevo/Medidor.cpp
+++ aire_nuevo/Medidor.cpp
@@ -14,7 +14,7 @@ Medidor::Medidor(void) {
   ledB_pin = 2;
   buzzer_pin = 7;
   pulsador_pin = 8;
-  numero_de_serie = "0430"; // 430 es el ultimo numero cargado
+  numero_de_serie = "0547 "; // #0546 -> ultima cargada 
 }
 
 void Medidor::iniciar() {
